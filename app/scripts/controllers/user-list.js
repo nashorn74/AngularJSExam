031236ad6ec73e8228982395246da1cc29c9577a
Jeong kum ho <nashorn74@gmail.com>
2017-10-14 13:59:43 +0900

    사용자 목록 조회 기능 추가

--- app/scripts/controllers/user-list.js
+++ app/scripts/controllers/user-list.js
@@ -8,10 +8,25 @@
  * Controller of the angularJsexamApp
  */
 angular.module('angularJsexamApp')
-  .controller('UserListCtrl', function () {
+  .controller('UserListCtrl', [
+  	"Data", "$scope", "$state", 
+  	function (Data, $scope, $state) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    //페이지가 로딩되었을 때 호출
+    $scope.$on('$viewContentLoaded', function() {
+    	$scope.requestUserList();
+    });
+    $scope.userList = [];
+    $scope.requestUserList = function() {
+    	var dataPromise = Data.getData(
+    		'http://127.0.0.1:52273/user');
+    	dataPromise.then(function(results) {
+    		$scope.userList = results.data;
+    	},function(reason){},function(update){});
+    }
+
+  }]);
